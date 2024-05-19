b7970e42a17841e383944d3d82e4dfd8791d1bf6
Thiago Rasquino <thiago.programador.net@gmail.com>
2024-05-19 14:45:59 -0300

    Adcionando o Prettier no projeto

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
-function Home(){
-    return <h1>Mih, eu te amo. Se você me ama também...dê uma risadinha.</h1>
+function Home() {
+  return <h1>Mih, eu te amo. Se você me ama também...dê uma risadinha.</h1>;
 }
 
 export default Home;

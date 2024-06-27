47ab7e026954a11d4f9fcfb0d606ba1f81c04f77
Thiago Rasquino <thiago.programador.net@gmail.com>
2024-06-27 14:03:27 -0300

    Acesso ssl no ambiente neon

--- infra/database.js
+++ infra/database.js
@@ -7,12 +7,15 @@ async function query(queryObject) {
     user: process.env.POSTGRES_USER,
     database: process.env.POSTGRES_DB,
     password: process.env.POSTGRES_PASSWORD,
+    ssl: process.env.NODE_ENV === "development" ? false : true
   });
   await client.connect();
   try {
     const result = await client.query(queryObject);
     return result;
   } catch (error) {
+    console.error(error);
+    throw error;
   } finally {
     await client.end();
   }

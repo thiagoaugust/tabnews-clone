fd57e9728b8e3e1743edf7500d348d6418bd0e22
Thiago Rasquino <thiago.programador.net@gmail.com>
2024-06-27 13:21:44 -0300

    Database + testes

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,9 +1,31 @@
 import database from "../../../../infra/database.js";
 
 async function status(request, response) {
-  const result = await database.query("SELECT 1 + 1 as sum;");
-  console.log(result.rows);
-  response.status(200).json({ status: "UP" });
+  const updatedAt = new Date().toISOString();
+
+  const databaseResultVersion = await database.query("SHOW server_version;");
+  const databaseVersion = databaseResultVersion.rows[0].server_version;
+
+  const databaseResultMaxConnections = await database.query("SHOW max_connections;");
+  const databaseMaxConnections = databaseResultMaxConnections.rows[0].max_connections;
+
+  const databaseName = process.env.POSTGRES_DB;
+  const databaseOpenedConnectionsResults = await database.query({
+    text: "SELECT count(*)::int FROM pg_stat_activity WHERE datname = $1;",
+    values:[databaseName]
+  });
+  const databaseOpenedConnections = databaseOpenedConnectionsResults.rows[0].count;
+
+  response.status(200).json({
+    updated_at: updatedAt,
+    dependecies: {
+      database: {
+        version: databaseVersion,
+        max_connections: parseInt(databaseMaxConnections),
+        opened_connections: databaseOpenedConnections
+      }
+    }
+  });
 }
 
 export default status;

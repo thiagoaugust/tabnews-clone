eb3e0fb279f1b78988e088c8d79917591c804b1e
Thiago Rasquino <thiago.programador.net@gmail.com>
2024-06-28 18:53:46 -0300

    Config controller migrations + testes

---
+++ pages/api/v1/migrations/index.js
@@ -0,0 +1,33 @@
+import migrationRunner from "node-pg-migrate";
+import { join } from "node:path";
+import database from "infra/database.js";
+
+export default async function migrations(request, response) {
+  const dbClient = await database.getNewClient();
+  const defaultMigrationsOptions = {
+    dbClient: dbClient,
+    dryRun: true,
+    dir: join("infra", "migrations"),
+    direction: "up",
+    verbose: true,
+    migrationsTable: "pgmigrations",
+  };
+
+  if (request.method === "GET") {
+    const pendingMigrations = await migrationRunner(defaultMigrationsOptions);
+    await dbClient.end();
+    return response.status(200).json(pendingMigrations);
+  }
+
+  if (request.method === "POST") {
+    const migratedMigrations = await migrationRunner({
+      ...defaultMigrationsOptions,
+      dryRun: false,
+    });
+    await dbClient.end();
+    const status = migratedMigrations.length > 0 ? 201 : 200;
+
+    return response.status(status).json(migratedMigrations);
+  }
+  return response.status(405).end();
+}

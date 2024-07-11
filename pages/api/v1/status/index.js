6bb682a81eb4e0a17dd7c90e0e6465d112b99912
Thiago Rasquino <thiago.programador.net@gmail.com>
2024-07-10 22:56:51 -0300

    Fixing liting

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -6,15 +6,19 @@ async function status(request, response) {
   const databaseResultVersion = await database.query("SHOW server_version;");
   const databaseVersion = databaseResultVersion.rows[0].server_version;
 
-  const databaseResultMaxConnections = await database.query("SHOW max_connections;");
-  const databaseMaxConnections = databaseResultMaxConnections.rows[0].max_connections;
+  const databaseResultMaxConnections = await database.query(
+    "SHOW max_connections;",
+  );
+  const databaseMaxConnections =
+    databaseResultMaxConnections.rows[0].max_connections;
 
   const databaseName = process.env.POSTGRES_DB;
   const databaseOpenedConnectionsResults = await database.query({
     text: "SELECT count(*)::int FROM pg_stat_activity WHERE datname = $1;",
-    values:[databaseName]
+    values: [databaseName],
   });
-  const databaseOpenedConnections = databaseOpenedConnectionsResults.rows[0].count;
+  const databaseOpenedConnections =
+    databaseOpenedConnectionsResults.rows[0].count;
 
   response.status(200).json({
     updated_at: updatedAt,
@@ -22,9 +26,9 @@ async function status(request, response) {
       database: {
         version: databaseVersion,
         max_connections: parseInt(databaseMaxConnections),
-        opened_connections: databaseOpenedConnections
-      }
-    }
+        opened_connections: databaseOpenedConnections,
+      },
+    },
   });
 }
 
